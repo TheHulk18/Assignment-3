dd219cb0896d53c5bd569a1c6a577f79a9b06ffe
TheHulk18 <152805459+TheHulk18@users.noreply.github.com>
2025-03-24 16:13:42 +0530

    Add files via upload
    I used functions & modules to complete this assignment.

---
+++ Task 2.py
@@ -0,0 +1,11 @@
+import math  # Importing the math module
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+natural_log = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root of {num}: {square_root}")
+print(f"Natural logarithm (log base e) of {num}: {natural_log}")
+print(f"Sine of {num} (in radians): {sine_value}")
